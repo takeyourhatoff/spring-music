8520ea5ca9dd946d16bf152ca102d1072da5abae
Paddy Steed <jarktasaa@gmail.com>
2017-09-22 09:17:54 +0100

    fix build.yml

--- src/main/java/org/cloudfoundry/samples/music/Application.java
+++ src/main/java/org/cloudfoundry/samples/music/Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer {
+SyntaxErrorpublic class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         new SpringApplicationBuilder(Application.class).
